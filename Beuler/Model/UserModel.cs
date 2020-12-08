039e393ecbc650edbb1cd9bdbc216e8d5750c7c8
unknown <bryonczaja@lewisu.edu>
2020-12-07 22:23:43 -0600

    initial

---
+++ Beuler/Model/UserModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Data.Sql;
+using System.ComponentModel;
+
+namespace Beuler.Model
+{
+    public class User
+    {
+        private string fName;
+        private string lName;
+        //Should be a hash
+        private string passWord;
+        private string address;
+
+        public string FirstName
+        {
+            get { return fName;}
+            set 
+            {
+                fName = value;
+            }
+        }
+        public string LastName
+        {
+            get { return lName;}
+            set 
+            {
+                lName = value;
+            }
+        }
+
+        public string Password
+        {
+            get { return passWord;}
+            set 
+            {
+                passWord = value;
+            }
+        } 
+
+        public string Address
+        {
+            get { return address; }
+            set
+            {
+                address = value;
+            }
+        }
+    }
+}
---
+++ Beuler/ViewModel/UserViewModel.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Beuler.Model;
+using System.Windows.Input;
+using System.ComponentModel;
+
+namespace Beuler.ViewModel
+{
+    class UserViewModel : INotifyPropertyChanged
+    {
+        private User user;
+    
+        public string FirstName { 
+            get {return user.FirstName;} 
+            set {
+                if(user.FirstName != value) {
+                    user.FirstName = value;
+                    OnPropertyChange("FirstName");
+                }
+            }
+        }
+
+        public string LastName { 
+            get {return user.LastName;} 
+            set {
+                if(user.LastName != value) {
+                    user.LastName = value;
+                    OnPropertyChange("LastName");
+                }
+            }
+        }
+        
+        public string Password { 
+            get {return user.Password;} 
+            set {
+                if(user.Password != value) {
+                    user.Password = value;
+                    OnPropertyChange("FirstName");
+                }
+            }
+        }
+
+        public string Address { 
+            get {return user.Address;} 
+            set {
+                if(user.Address != value) {
+                    user.Address = value;
+                    OnPropertyChange("Address");
+                }
+            }
+        }
+
+        public Boolean ComparePassword(string password)
+        {
+            string strFromDatabsae = "";
+            if(String.Compare(password, strFromDatabsae) == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public Boolean RegisterUser()
+        {
+            //Check if user exists
+            //If not regiser user
+            //Otherwise
+            return false;
+        }
+
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChange(string property)
+        {
+            if(this.PropertyChanged != null)
+            {
+                var e = new PropertyChangedEventArgs(property);
+                this.PropertyChanged(this, e);
+            }
+        }
+    }
+
+    public class DelegateCommand : ICommand
+    {
+       private readonly Action _action;
+
+       public DelegateCommand(Action action)
+       {
+            _action = action;
+       }
+
+       public void Execute(object parameter)
+       {
+            _action();
+       }
+
+       public bool CanExecute(object parameters)
+       {
+            return true;
+       }
+    }
+
+}
